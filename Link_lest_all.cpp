02a0dbb97912be24f39b6f38adf3fc93c5560645
suyab-amin <suyabamins@gmail.com>
2025-09-15 23:25:28 +0600

    added marge sorte

---
+++ Link_lest_all.cpp
@@ -0,0 +1,157 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int value;
+    node*next;
+};
+node*head=NULL;
+
+void insert_begain(int n){
+  node*newItem=new node();
+   newItem->value=n;
+   newItem->next=head;
+   head=newItem;
+}
+
+void Print(){
+    node*temp=head;
+    while (temp!=NULL)
+    {
+        cout<<temp->value<<"->";
+        temp=temp->next;
+    }
+    cout<<" "<<endl;
+    
+
+}
+void insert_last(int v){
+     node*newItem=new node();
+     node*temp=head;
+     newItem->value=v;
+     while (temp->next!=NULL)
+     {
+        temp=temp->next;
+     }
+     temp->next=newItem;
+     newItem->next=NULL;
+     
+}
+void insert_after(int v,int n){
+    node*newItem=new node();
+    node*temp=head;
+    if(head==NULL){
+        insert_begain(v);
+    }
+    while (temp!=NULL&&temp->value!=n)
+    {
+        temp=temp->next;
+    }
+    newItem->value=v;
+    newItem->next=temp->next;
+    temp->next=newItem;
+    
+}
+void deleteFirst(){
+    node*temp=head;
+    head=head->next;
+    delete(temp);
+}
+void delete_after(int n){
+    node*temp=head;
+    while (temp!=NULL&& temp->value!=n)
+    {
+      temp=temp->next;
+    }
+    temp->next=temp->next->next;
+    
+}
+int size(){
+    node*temp=head;
+    int count=0;
+    while (temp!=NULL)
+    {
+        temp=temp->next;
+        count++;
+    }
+    return count;
+    
+}
+void insert_nth(int v,int n){
+    node*newItem=new node();
+        node*temp=head;
+     for(int i=0;i<n-1;i++){
+        temp=temp->next;
+     }
+     newItem->value=v;
+     newItem->next=temp->next;
+     temp->next=newItem;
+     
+ }
+
+void manue(){
+    cout<<"------enter any choice--------"<<endl;
+    cout<<"1)insert first"<<endl;
+    cout<<"2)print link lest"<<endl;
+    cout<<"3)insert last"<<endl;
+    cout<<"4)inser after"<<endl;
+     cout<<"5)delete first"<<endl;
+      cout<<"6)delete after"<<endl;
+      cout<<"7)size"<<endl;
+      cout<<"8)insert after"<<endl;
+    cout<<"0)quit"<<endl;
+}
+
+int main(){
+     manue();
+    while (1)
+    {
+         cout<<"enter any choice---";
+       int n;
+        cin>>n;
+      if(n==1){
+        cout<<"enter value--";
+        int v;
+        cin>>v;
+        insert_begain(v);
+      }if(n==3){
+        cout<<"enter value--";
+        int v;
+        cin>>v;
+        insert_last(v);
+      }if(n==4){
+        cout<<"enter after positio-";
+        int n;
+        cin>>n;
+        cout<<"enter value--";
+        int v;
+        cin>>v;
+       insert_after(v,n);
+      }else if(n==5){
+       deleteFirst();
+      }if(n==6){
+        cout<<"delete after- ";
+        int n;
+        cin>>n;
+       delete_after(n);
+      }if(n==7){
+      int s= size();
+      cout<<endl<<"size is--"<<s<<endl;
+      }if(n==8){
+         cout<<"enter nth--";
+       int n;
+        cin>>n;
+         cout<<"enter value--";
+        int v;
+        cin>>v;
+       insert_nth(v,n); 
+      }
+      else if(n==2){
+        Print();
+      }else if(n==0){
+        break;
+      }
+    }
+    
+}
---
+++ margesort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void merge(vector<int>&arr,int st,int mid,int end){
+  vector<int>temp;
+  int i=st,j=mid+1;
+  while (i<=mid && j<=end)
+  {
+    if(arr[i]<=arr[j]){
+        temp.push_back(arr[i]);
+        i++;
+    }else{
+        temp.push_back(arr[j]);
+        j++;
+    }
+  }
+  while (i<=mid)
+  {
+    temp.push_back(arr[i]);
+    i++;
+  }
+  while (j<=end)
+  {
+   temp.push_back(arr[j]);
+   j++;
+  }
+  for(int idx=0;idx<temp.size();idx++){
+    arr[idx+st]=temp[idx];
+
+  }
+  
+  
+}
+
+void mergeSort(vector<int>&arr,int st,int end){
+    if(st<end){
+        int mid=st+(end-st)/2;
+
+        mergeSort(arr,st,mid); //left
+        mergeSort(arr,mid+1,end); //right
+
+        merge(arr,st,mid,end);
+    }
+}
+
+
+int main(){
+    vector<int>arr={10,31,35,8,32,17};
+    mergeSort(arr,0,arr.size()-1);
+    for(int val:arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
